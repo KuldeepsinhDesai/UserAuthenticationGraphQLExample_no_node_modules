f64afb17d88d0b9f628d607dfbccc45d4c2e9700
Kuldeep Desai <kuldeepdesai66@gmail.com>
2023-03-17 13:00:04 -0400

    initial commit

---
+++ react-client/src/App.js
@@ -0,0 +1,70 @@
+import './App.css';
+//
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  Routes
+} from "react-router-dom";
+//
+// This app requires react-bootstrap and bootstrap installed: 
+//  npm install react-bootstrap bootstrap
+//
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import './App.css';
+//
+import UserList from './components/UserList';
+import CreateUser from './components/CreateUser';
+
+import Login from './components/Login';
+import Home from './components/Home';
+
+//
+function App() {
+
+  return (
+    <Router>
+      
+      <Navbar bg="primary" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="home">React Client For GraphQL API</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link as={Link} to="/home" >Home</Nav.Link>
+              <Nav.Link as={Link} to="/login">Login</Nav.Link>
+              <Nav.Link as={Link} to="/createuser">Create User</Nav.Link>
+              <Nav.Link as={Link} to="/userlist">User List</Nav.Link>
+              
+
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <div>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path = "home" element={<Home />} /> 
+          <Route path="login" element= {< Login />}  />
+          <Route path = "userlist" element={<UserList />} />
+          <Route path = "createuser" element={<CreateUser />} />
+          
+
+        </Routes>
+    </div>
+      
+      
+
+    </Router>
+
+
+  );
+}
+//<Route render ={()=> < App />} path="/" />
+export default App;
